7607c323c7217c3e097638bcd9d2ee14bca1ba63
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-15 16:32:32 +0900

    other event listener

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter(){
-    title.innerText = "mouse is here!";
+    h1.innerText = "mouse is here!";
 }
 
 function handleMouseLeave(){
-    title.innerText = "mouse is gone!"; 
+    h1.innerText = "mouse is gone!"; 
 }
 
 function handleWindowResize(){
     document.body.style.backgroundColor = "tomato";
 }
 
-title.onclick = handleTitleClick; 
-title.addEventListener("mouseenter", handleMouseEnter); 
-title.addEventListener("mouseleave", handleMouseLeave); 
+function handleWindowCopy(){
+    alert("copier!")
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI!"); 
+}
+
+function handleWindowOnline(){
+    alert("Now connected!"); 
+}
+
+h1.addEventListener("click", handleTitleClick)
+h1.addEventListener("mouseenter", handleMouseEnter); 
+h1.addEventListener("mouseleave", handleMouseLeave); 
 
 window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline); 
+window.addEventListener("online", handleWindowOnline); 
