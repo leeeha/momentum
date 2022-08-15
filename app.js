cf7a11c524966d9231f8ee18d8f008dd54fe5db8
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-15 16:07:57 +0900

    mouse event listener

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick(){
-    console.log("title was clicked!")
+    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    title.innerText = "mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "mouse is gone!"; 
 }
 
 title.addEventListener("click", handleTitleClick); 
+title.addEventListener("mouseenter", handleMouseEnter); 
+title.addEventListener("mouseleave", handleMouseLeave); 
 
