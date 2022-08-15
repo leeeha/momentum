3c725d5e6a6d013c97b5d59895641f6f3ac00dba
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-15 15:42:02 +0900

    click event listener

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const title = document.querySelector("div.hello:first-child h1");
-title.style.color = "blue";
+
+function handleTitleClick(){
+    console.log("title was clicked!")
+}
+
+title.addEventListener("click", handleTitleClick); 
+
