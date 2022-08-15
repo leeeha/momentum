f6a5536fb6e57e44cdd70d3de4b714511657878a
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-15 13:18:50 +0900

    function 기본

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert("hi!");
+const calculator = {
+    plus: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    multiply: function(a, b){
+        console.log(a * b);
+    },
+    divide: function(a, b){
+        console.log(a / b);
+    },
+    power: function(a, b){
+        console.log(a ** b); 
+    }
+}
+
+calculator.plus(3, 4);
+calculator.minus(3, 4);
+calculator.multiply(3, 4);
+calculator.divide(3, 4);
+calculator.power(3, 4);
