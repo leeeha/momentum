c387ebc3bf0e8e4e20ab08bd831ae86bebae98ff
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-15 15:19:28 +0900

    querySelector, getElementById의 차이점

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector("#hello h1");
+const title2 = document.getElementById("hello");
 console.log(title);
+console.log(title2);
