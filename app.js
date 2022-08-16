1aee8fc0a02a0d66233036bb2d8e29f9008cf5b1
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-16 21:21:17 +0900

    form의 input 태그 속성으로 입력값 조정하기

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
     const username = loginInput.value; 
-    if(username === ""){
-        alert("Please write your name!");
-    }else if(username.length > 15){
-        alert("Your name is too long.");
-    }else{
-        console.log(username); 
-    }
+    console.log(username); 
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
