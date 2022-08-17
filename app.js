5dca17f6eabee70e05ff3c6442c320f1e14b1b10
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-17 22:13:02 +0900

    submit, link click에 대한 브라우저의 기본 동작 막기

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a")
 
+// 방금 일어난 이벤트에 대한 정보를 담은 event 객체 
 function onLoginSubmit(event){
-    // submit 했을 때 페이지가 새로고침 되지 않도록 
+    // 브라우저의 기본 동작을 막는다.
     event.preventDefault(); 
-    console.log(loginInput.value);
+    console.log(event);
 }
 
+function handleLinkClick(event){
+    event.preventDefault(); 
+    console.dir(event); 
+}
+
+// 이벤트가 발생하면 브라우저가 함수를 실행시킴. 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
 
