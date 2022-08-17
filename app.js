daaff44c600de7046ada7eeb596a34d10c260679
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-17 22:35:09 +0900

    css class로 visibility 조절하기

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a")
+const greeting = document.querySelector("#greeting")
 
-// 방금 일어난 이벤트에 대한 정보를 담은 event 객체 
+const HIDDEN_CLASSNAME = "hidden";
+
 function onLoginSubmit(event){
-    // 브라우저의 기본 동작을 막는다.
     event.preventDefault(); 
-    console.log(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value; 
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
+    greeting.innerText = `Hello ${username}`;
 }
 
-function handleLinkClick(event){
-    event.preventDefault(); 
-    console.dir(event); 
-}
-
-// 이벤트가 발생하면 브라우저가 함수를 실행시킴. 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
