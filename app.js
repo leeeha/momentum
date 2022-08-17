02bd169820324c58bc43e3c1960b309c6b9a2cc0
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-17 23:17:17 +0900

    input에 입력된 값을 로컬 스토리지에 저장하기

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); 
+    // form 화면에 표시하기 
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
+    // input에 입력된 값을 로컬 스토리지에 저장하기 
     const username = loginInput.value; 
-    greeting.classList.remove(HIDDEN_CLASSNAME); 
-    greeting.innerText = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username); 
+
+    // greeting 화면에 표시하기 
+    paintGreetings(username); 
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form 
+    loginForm.classList.remove(HIDDEN_CLASSNAME); 
+    loginForm.addEventListener("submit", onLoginSubmit); 
+}else{
+    // show the greeting 
+    paintGreetings(savedUsername); 
+}
