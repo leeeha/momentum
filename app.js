2c6738f42cf21c5aaa0cbe48a020952572564e1a
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-17 21:58:01 +0900

    submit 했을 때 페이지 새로고침 되지 않도록

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button"); 
 
-function onLoginBtnClick(){
-    const username = loginInput.value; 
-    console.log(username); 
+function onLoginSubmit(event){
+    // submit 했을 때 페이지가 새로고침 되지 않도록 
+    event.preventDefault(); 
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
