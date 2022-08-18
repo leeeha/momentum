c14be53904e7baee7d5d66f8ca84cf1d042f5f38
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-18 22:37:27 +0900

    setInterval

--- js/greeting.js
+++ js/greeting.js
@@ -6,15 +6,15 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
-    event.preventDefault(); 
-    // form 화면에 표시하기 
+    event.preventDefault();
+    // form 감추기 
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     // input에 입력된 값을 로컬 스토리지에 저장하기 
     const username = loginInput.value; 
     localStorage.setItem(USERNAME_KEY, username); 
 
-    // greeting 화면에 표시하기 
+    // greeting 표시하기 
     paintGreetings(username); 
 }
 
@@ -23,12 +23,15 @@ function paintGreetings(username){
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+// 로컬 스토리지에 저장된 값 가져오기 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
     // show the form 
-    loginForm.classList.remove(HIDDEN_CLASSNAME); 
-    loginForm.addEventListener("submit", onLoginSubmit); 
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+
+    // form을 제출하면 onLoginSubmit 함수 실행 
+    loginForm.addEventListener("submit", onLoginSubmit);
 }else{
     // show the greeting 
     paintGreetings(savedUsername); 
