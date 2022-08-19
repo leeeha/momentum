60c9eef0b8267b795fa6ab9f6198fbebf0c05020
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-19 18:41:42 +0900

    인용문구 랜덤으로 생성하기

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,7 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock(){
-    const date = new Date(); // 매초마다 객체 생성
+    const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0"); 
     const minutes = String(date.getMinutes()).padStart(2, "0"); 
     const seconds = String(date.getSeconds()).padStart(2, "0");
@@ -9,4 +9,4 @@ function getClock(){
 }
 
 getClock();
-setInterval(getClock, 1000); 
+setInterval(getClock, 1000); // 1초마다 함수 실행
