e421bb91939aea178b0fca4d073359497fdd9123
leeeha <jxlhe46@seoultech.ac.kr>
2022-08-21 20:31:25 +0900

    delete item

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    // target은 버튼, parent는 li 
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span); // li 안에 span 추가하기 
     span.innerText = newTodo; 
-    toDoList.appendChild(li); // ul에 li 추가하기 
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    
+    li.appendChild(span); // li에 span 추가
+    li.appendChild(button); // li에 button 추가
+    toDoList.appendChild(li); // ul에 li 추가 
 }
 
 function handleToDoSubmit(event){
