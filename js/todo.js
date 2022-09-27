a3bc484fa12cdb12b077414ad06819f56c8c722a
leeeha <jxlhe46@seoultech.ac.kr>
2022-09-27 19:16:30 +0900

    로컬 스토리지에도 항목 제거 반영하기

--- js/todo.js
+++ js/todo.js
@@ -7,21 +7,30 @@ const TODOS_KEY = "todos"
 let toDos = []; 
 
 function saveToDos(){
-    // 로컬 스토리지에 배열을 문자열 형태로 추가  
+    // 로컬 스토리지에 배열을 문자열 형태로 추가 
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
 }
 
 function deleteToDo(event){
     // target은 버튼, parent는 li 
     const li = event.target.parentElement;
-    li.remove(); // 항목 제거 
+    li.remove(); 
+
+    // 선택된 li.id와 다른 toDo는 남겨두기 
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+
+    // 로컬 스토리지에 변경된 배열 내용 반영하기 
+    saveToDos(); 
 }
 
 function paintToDo(newTodo){
-    // li 태그의 span에 새 항목 추가 
+    // 새 항목을 구별하기 위해 id 추가 
     const li = document.createElement("li");
+    li.id = newTodo.id; 
+    
+    // span 태그의 텍스트 설정 
     const span = document.createElement("span");
-    span.innerText = newTodo; 
+    span.innerText = newTodo.text; 
     
     // x 버튼 클릭하면 항목 제거 
     const button = document.createElement("button");
@@ -35,14 +44,16 @@ function paintToDo(newTodo){
 }
 
 function handleToDoSubmit(event){
-    event.preventDefault();
-
+    event.preventDefault(); // 페이지 새로고침 방지 
     const newTodo = toDoInput.value; 
     toDoInput.value = ""; 
 
-    // 초기에 toDos 배열은 항상 비어있음. 
-    toDos.push(newTodo); // 배열에 추가 
-    paintToDo(newTodo); // 브라우저에 표시 
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    };
+    toDos.push(newTodoObj); // 배열에 객체 저장 
+    paintToDo(newTodoObj); // 브라우저에 새 항목 표시 
     saveToDos(); // 로컬 스토리지에 배열 저장 
 }
 
@@ -57,6 +68,6 @@ if(savedToDos !== null){
     // 로컬 스토리지에 저장된 값으로 toDos 배열 초기화 
     toDos = parsedToDos; 
 
-    // 배열의 각 항목에 대해 동일한 함수 실행 
+    // 배열의 각 항목에 대해 paintToDo 함수 실행 
     parsedToDos.forEach(paintToDo); 
 }
